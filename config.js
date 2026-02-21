818455fd4ef1e8c1ee7468a5852a1e9953701634
mjersey <mjjersey06@gmail.com>
2026-02-21 22:43:10 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB-w71mc4au-fRHybN17gw34PlbRzXeaWc",
+    authDomain: "trackly-log.firebaseapp.com",
+    projectId: "trackly-log",
+    storageBucket: "trackly-log.firebasestorage.app",
+    messagingSenderId: "16616558944",
+    appId: "1:16616558944:web:266ad960b8141ce16c4e05"
+};
